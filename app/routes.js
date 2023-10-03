a5c2e0ef476e7dd4992b48b062bf0c6b5ceb644b
Adam Silver <adambsilver@gmail.com>
2023-10-03 14:33:46 +0100

    Flesh out rest of the journey

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,11 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 const _ = require('lodash')
 
+router.all('*', (req, res, next) => {
+  res.locals.query = req.query
+  next()
+})
+
 router.post('/data-subject', (req, res) => {
   if(_.get(req, 'body.check.dataSubject') == 'Listed building') {
     res.redirect('/upload')
@@ -52,5 +57,29 @@ router.post('/dataset', (req, res) => {
 })
 
 router.post('/upload', (req, res) => {
+  _.set(req.session.data, 'hasErrors', !_.get(req.session.data, 'hasErrors'))
   res.redirect('/results')
+})
+
+router.post('/results', (req, res) => {
+  res.redirect('/download')
+})
+
+router.post('/download', (req, res) => {
+  res.redirect('/email-address')
+})
+
+router.post('/email-address', (req, res) => {
+  res.redirect('/url')
+})
+
+router.post('/url', (req, res) => {
+  res.redirect('/check')
+})
+
+router.post('/check', (req, res) => {
+
+  delete req.session.data.check
+
+  res.redirect('/confirmation')
 })
