f0df965430183d0aedae62180a3eb2a40bb0cb59
Adam Silver <adambsilver@gmail.com>
2023-10-03 11:01:00 +0100

    Add data subject and dataset pages

--- app/routes.js
+++ app/routes.js
@@ -5,5 +5,52 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
+const _ = require('lodash')
 
-// Add your routes here
+router.post('/data-subject', (req, res) => {
+  if(_.get(req, 'body.check.dataSubject') == 'Listed building') {
+    res.redirect('/results')
+  } else {
+    res.redirect('/upload')
+  }
+})
+
+router.get('/dataset', (req, res) => {
+  let options = [
+    { text: 'Article 4 direction dataset' },
+    { text: 'Article 4 direction area dataset' }
+  ]
+
+  switch(_.get(req, 'session.data.check.dataSubject')) {
+    case 'Conservation area':
+      options = [
+        { text: 'Conservation area dataset'},
+        { text: 'Conservation area document dataset'}
+      ]
+      break
+    case 'Tree preservation order':
+      options = [
+        { text: 'Tree preservation order dataset'},
+        { text: 'Tree preservation zone dataset'},
+        { text: 'Tree dataset'}
+      ]
+      break
+  }
+
+  options = options.map(option => {
+    option.value = option.text
+    return option
+  })
+
+  res.render('dataset', {
+    options
+  })
+})
+
+router.post('/dataset', (req, res) => {
+  res.redirect('/upload')
+})
+
+router.post('/upload', (req, res) => {
+  res.redirect('/results')
+})
