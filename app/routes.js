de5ea9fffdeeb824e1ae355ec698b9ba8f288b6e
Adam Silver <adambsilver@gmail.com>
2023-10-18 11:55:51 +0100

    Add realistic data

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 const _ = require('lodash')
+const camden = require('../app/data/camden.json')
 
 router.all('*', (req, res, next) => {
   res.locals.query = req.query
@@ -55,6 +56,42 @@ router.post('/upload', (req, res) => {
   res.redirect('/errors')
 })
 
+router.get('/errors', (req, res) => {
+  let rows = camden.slice(0, 10);
+
+  rows = rows.map(row => {
+    const newRow = {};
+    for (const key in row) {
+      newRow[key] = { value: row[key] };
+    }
+    return newRow;
+  })
+
+  rows.map(row => {
+    let newRow = row
+    let errorType = _.sample(['Reference missing', 'Start date must be today or in the past', 'Location not in England'])
+
+    switch(errorType) {
+      case 'Reference missing':
+        newRow.Reference.value = ''
+        newRow.Reference.error = errorType
+        break
+      case 'Start date must be today or in the past':
+        newRow['Start date'].value = '01/12/2025'
+        newRow['Start date'].error = errorType
+        break
+      case 'Location not in England':
+        newRow['Geometry'].error = errorType
+        break
+    }
+    return newRow
+  })
+
+  res.render('errors', {
+    rows
+  })
+})
+
 router.post('/errors', (req, res) => {
   if(req.session.data.check.fixErrors == 'Yes') {
     res.redirect('/upload')
@@ -64,7 +101,8 @@ router.post('/errors', (req, res) => {
 })
 
 router.post('/transformations', (req, res) => {
-  res.redirect('/download')
+  // res.redirect('/download')
+  res.redirect('/has-webpage')
 })
 
 router.post('/download', (req, res) => {
