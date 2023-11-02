adbc16d3b8baceb98a819c96605dcc2c85325b1e
Adam Silver <adambsilver@gmail.com>
2023-11-02 14:21:02 +0000

    Simplify flow

--- app/routes.js
+++ app/routes.js
@@ -54,124 +54,87 @@ router.post('/dataset', (req, res) => {
 })
 
 router.post('/upload', (req, res) => {
-  res.redirect('/errors')
+  res.redirect('/results')
 })
 
-router.get('/errors', (req, res) => {
-  let rows = camden.slice(0, 10);
+router.get('/results', (req, res) => {
+  if(req.query.noErrors == 'true') {
 
-  rows = rows.map(row => {
-    const newRow = {};
-    for (const key in row) {
-      newRow[key] = { value: row[key] };
-    }
-    return newRow;
-  })
+    let rows = camden.map(row => {
+      const newRow = {};
+      for (const key in row) {
+        newRow[key] = { value: row[key] };
+      }
+      return newRow;
+    })
 
-  rows.map(row => {
-    let newRow = row
-    let errorType = _.sample(['Reference missing', 'Start date must be today or in the past', 'Location not in England'])
-
-    switch(errorType) {
-      case 'Reference missing':
-        newRow.Reference.value = ''
-        newRow.Reference.error = errorType
-        break
-      case 'Start date must be today or in the past':
-        newRow['Start date'].value = '01/12/2025'
-        newRow['Start date'].error = errorType
-        break
-      case 'Location not in England':
-        newRow['Geometry'].error = errorType
-        break
-    }
-    return newRow
-  })
-
-  const locationErrorCount = rows.reduce((acc, row) => {
-    if (row.Geometry.error) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0);
-
-  const referenceErrorCount = rows.reduce((acc, row) => {
-    if (row.Reference.error) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0);
-
-  const startDateErrorCount = rows.reduce((acc, row) => {
-    if (row['Start date'].error) {
-      return acc + 1;
-    }
-    return acc;
-  }, 0);
-
-  res.render('errors', {
-    rows,
-    referenceErrorCount,
-    locationErrorCount,
-    startDateErrorCount
-  })
-})
-
-router.post('/errors', (req, res) => {
-  if(req.session.data.check.fixErrors == 'Yes') {
-    res.redirect('/upload')
+    res.render('no-errors', {
+      rows
+    })
   } else {
-    res.redirect('/transformations')
+    let rows = camden.slice(0, 10);
+
+    rows = rows.map(row => {
+      const newRow = {};
+      for (const key in row) {
+        newRow[key] = { value: row[key] };
+      }
+      return newRow;
+    })
+
+    rows.map(row => {
+      let newRow = row
+      let errorType = _.sample(['Reference missing', 'Start date must be today or in the past', 'Location not in England'])
+
+      switch(errorType) {
+        case 'Reference missing':
+          newRow.Reference.value = ''
+          newRow.Reference.error = errorType
+          break
+        case 'Start date must be today or in the past':
+          newRow['Start date'].value = '01/12/2025'
+          newRow['Start date'].error = errorType
+          break
+        case 'Location not in England':
+          newRow['Geometry'].error = errorType
+          break
+      }
+      return newRow
+    })
+
+    const locationErrorCount = rows.reduce((acc, row) => {
+      if (row.Geometry.error) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+
+    const referenceErrorCount = rows.reduce((acc, row) => {
+      if (row.Reference.error) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+
+    const startDateErrorCount = rows.reduce((acc, row) => {
+      if (row['Start date'].error) {
+        return acc + 1;
+      }
+      return acc;
+    }, 0);
+    res.render('errors', {
+      rows,
+      referenceErrorCount,
+      locationErrorCount,
+      startDateErrorCount
+    })
   }
+
 })
 
-router.get('/transformations', (req, res) => {
-  let rows = camden.slice(-20);
-
-  rows = rows.map(row => {
-    const newRow = {};
-    for (const key in row) {
-      newRow[key] = { value: row[key] };
-    }
-    return newRow;
-  })
-  .map(row => {
-    let date = DateTime.fromFormat(row['Start date'].value, 'dd/MM/yyyy')
-    row['Start date'].value = date.toFormat('yyyy-MM-dd')
-    return row
-  })
-  .map(row => {
-    row['Entry date'] = {
-      value: DateTime.local().toFormat('yyyy-MM-dd')
-    }
-    return row
-  })
-
-  res.render('transformations', {
-    rows
-  })
+router.post('/results', (req, res) => {
+  res.redirect('/results')
 })
-
-router.post('/transformations', (req, res) => {
-  res.redirect('/has-webpage')
-})
-
-router.post('/download', (req, res) => {
-  res.redirect('/has-webpage')
-})
-
-router.post('/has-webpage', (req, res) => {
-  if(req.session.data.check.hasURL == 'Yes') {
-    res.redirect('/url')
-  } else {
-    res.redirect('/no-webpage')
-  }
-})
-
-router.post('/url', (req, res) => {
-  res.redirect('/email-address')
-})
-
 
 router.post('/email-address', (req, res) => {
   res.redirect('/name')
@@ -182,7 +145,6 @@ router.post('/name', (req, res) => {
 })
 
 router.post('/name', (req, res) => {
-  // res.redirect('/start-date')
   res.redirect('/lpa')
 })
 
@@ -195,6 +157,5 @@ router.post('/lpa', (req, res) => {
 })
 
 router.post('/check', (req, res) => {
-  delete req.session.data.check
   res.redirect('/confirmation')
 })
